0858b9701047db93909ccd7f09c8d7a9a96c4213
Kahuan <kahuanvitelli@gmail.com>
2023-07-27 17:05:25 -0300

    1.1.0

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -9,28 +9,28 @@ export default function Rodape() {
     <footer className={styles.rodape}>
       <div className={styles.rodape__icones}>
         <a
-          href="https://www.facebook.com/AluraCursosOnline/"
+          href="https://facebook.com"
           target="_blank"
           rel="noreferrer"
         >
           <img src={facebook} alt="ícone do facebook" />
         </a>
         <a
-          href="https://twitter.com/aluraonline?lang=en"
+          href="https://twitter.com"
           target="_blank"
           rel="noreferrer"
         >
           <img src={twitter} alt="ícone do twitter" />
         </a>
         <a
-          href="https://www.instagram.com/aluraonline/?hl=en"
+          href="https://www.instagram.com"
           target="_blank"
           rel="noreferrer"
         >
           <img src={instagram} alt="ícone do instagram" />
         </a>
       </div>
-      <p>Desenvolvido por Alura</p>
+      <p>Desenvolvido por Kahuan e Alura</p>
     </footer>
   );
 }
